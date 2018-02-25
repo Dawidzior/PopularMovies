98587862219eb1606a72288361e3f27f36f318ef
Dawid <klembix@o2.pl>
2018-02-25 17:58:39 +0100

    Added libraries, constants from guide, recycler view.

--- app/src/main/java/dawidzior/popularmovies/MainActivity.java
+++ app/src/main/java/dawidzior/popularmovies/MainActivity.java
@@ -1,31 +1,29 @@
 package dawidzior.popularmovies;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+    private static final String RRECOMMENDED_SIZE = "w185";
+    private static final String HTTP_REQUEST_URL_POPULAR = "http://api.themoviedb.org/3/movie/popular?api_key=";
+
+    @BindView(R.id.movies_list)
+    protected RecyclerView moviesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
